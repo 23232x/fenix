2e078e20d8d7c72c1a1bb66267699607cb4a58ce
23232x <23232x@gmail.com>
2021-05-17 13:21:46 -0300

    Adicionado pagina portifolio e historia

---
+++ parallax.js
@@ -0,0 +1,10 @@
+$(".separador").hover(function() {
+    $(this).next(".separador-container").show(); //hover in
+}, function() {
+    $(this).next(".separador-container").hide(); //hover out
+});
+/* $(".fixed-parallax").hover(function() {
+    $(this).next(".contend .contend-description").show(); //hover in
+}, function() {
+    $(this).next(".contend .contend-description").hide(); //hover out
+});*/
